7f0d62a60de173d38f8d9d12006e3e2cf0d09167
Pollyana <131013385+PollyanaSte@users.noreply.github.com>
2026-09-24 20:56:26 -0300

    atualizando

--- interface/gerency/src/components/UserTr.tsx
+++ interface/gerency/src/components/UserTr.tsx
@@ -26,7 +26,7 @@ import './UserTr.css'
 
         const handleDelete = async() => {
           try{
-            const response = await fetch(`https://controle-de-gastos-ur8h.onrender.com/transacoes/${transacao.id}`, {
+            const response = await fetch(`https://controle-de-gastos-ur8h.onrender.com/${transacao.id}`, {
               method: 'DELETE',
             });
             if (response.ok) {
